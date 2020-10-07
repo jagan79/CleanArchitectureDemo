c6e0c39b3232a31620bee6c98be91b515d41458b
Jeremie Agan <Jeremie.D.Agan@gds.ey.com>
2020-10-07 12:18:24 +0800

    Add Course Domain Model

---
+++ CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImageUrl { get; set; }
+    }
+}
+    
