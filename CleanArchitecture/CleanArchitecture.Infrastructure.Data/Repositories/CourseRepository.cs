edbe3169661f8f691057d2462bb528c21f3f66c9
Jeremie Agan <Jeremie.D.Agan@gds.ey.com>
2020-10-19 14:44:28 +0800

    Added following changes 1. Commands 2. CommandHandlers 3. Updated Course Repository 4. Cleaned up IoC

--- CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -15,6 +15,13 @@ namespace CleanArchitecture.Infrastructure.Data.Repositories
         {
             _context = context;
         }
+
+        public void Add(Course course)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
+
         public IEnumerable<Course> GetCourses()
         {
             return _context.Courses;
